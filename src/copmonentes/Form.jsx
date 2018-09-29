067f6f705b51174a4c3fad7f3902e15e9ab13f1d
majed187 <majedshrndan45@gmail.com>
2018-09-29 23:44:26 +0200

    added the search form

---
+++ src/copmonentes/Form.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+class Form extends Component {
+  eventHandler = e => {
+    e.preventDefault();
+    const recipeName = e.target.elements.recipeName.value;
+    console.log(recipeName);
+    this.props.getRecipe(recipeName);
+  };
+  render() {
+    return (
+      <div className="container">
+        <h2>Search your favorite Food</h2>
+        <form
+          className="form input-group input-group-lg my-5   "
+          onSubmit={this.eventHandler}
+        >
+          <input
+            type="text"
+            className="form-control h4"
+            placeholder="Enter your City "
+            name="recipeName"
+            required
+          />
+
+          <div className="input-group-append">
+            <button className="btn btn-outline-secondary " type="submit">
+              <FontAwesomeIcon className=" fa-spin" icon={faSpinner} />
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
