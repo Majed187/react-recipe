2a71a7e2220a854d2221ebcff3e513352cad5aec
majed187 <majedshrndan45@gmail.com>
2018-09-30 13:17:10 +0200

    added Link button to the singel page recipe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Form from "./copmonentes/Form";
 
 import Recipes from "./copmonentes/Recipes";
 
-const API_KEY = "7390c7e600a58001fa5c48ec5a36ba7e";
+const API_KEY = "969e30909b4857ca9a32db801b10ed89";
 const baseURL = `https://cors-anywhere.herokuapp.com/https://food2fork.com/api/`;
 
 class App extends Component {
@@ -12,24 +12,34 @@ class App extends Component {
   };
 
   getRecipe = async recipeName => {
-    try {
-      const endPoint = `${baseURL}search?key=${API_KEY}
+    const endPoint = `${baseURL}search?key=${API_KEY}
 &q=${recipeName}`;
 
-      const api = await fetch(endPoint);
-      const data = await api.json();
-      this.setState({
-        recipes: data.recipes
-      });
-      console.log(this.state.recipes);
-    } catch (e) {
-      console.log(e);
-    }
+    const api = await fetch(endPoint);
+    const data = await api.json();
+
+    this.setState({
+      recipes: data.recipes
+    });
+    console.log(this.state.recipes);
+  };
+  componentDidMount = () => {
+    const json = localStorage.getItem("recipes");
+    const recipes = JSON.parse(json);
+    this.setState({ recipes });
+  };
+
+  componentDidUpdate = () => {
+    const recipes = JSON.stringify(this.state.recipes);
+    localStorage.setItem("recipes", recipes);
   };
 
   render() {
     return (
       <div className=" container App">
+        <h1 className="mt-5 text-center text-white ">
+          Search your favorite Food
+        </h1>
         <Form getRecipe={this.getRecipe} />
         <Recipes recipes={this.state.recipes} />
       </div>
